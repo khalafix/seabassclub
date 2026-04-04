e738071a563b0ae79e38dcf1a545bc9523526eb3
khalafix <bahman.esh@gmail.com>
2026-04-04 18:29:37 +0400

    change the menu

--- src/app/(blog)/blog-grid/page.tsx
+++ src/app/(blog)/blog-grid/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";  
+export const dynamic = "force-static";  
 
 import BlogGrid from "@/components/BlogGrid";
 
--- src/layouts/headers/Navmenu.tsx
+++ src/layouts/headers/Navmenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-//import menu_data from "@/data/seabass-menu-data";
-import menu_data from "@/data/menu-data";
+import menu_data from "@/data/seabass-menu-data";
+//import menu_data from "@/data/menu-data";
 import Link from "next/link";
 import { useState } from "react";
 
